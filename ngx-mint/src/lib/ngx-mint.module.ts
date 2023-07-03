89b18bb3627c079649cb4ef56d5226ad567239f5
Samuel T Underwood <sam@underwood.systems>
2023-07-02 23:48:31 -0400

    deleted boilerplate

--- ngx-mint/src/lib/ngx-mint.module.ts
+++ ngx-mint/src/lib/ngx-mint.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
-import { NgxMintComponent } from './ngx-mint.component';
-
-
 
 @NgModule({
   declarations: [
-    NgxMintComponent
+
   ],
   imports: [
   ],
   exports: [
-    NgxMintComponent
+
   ]
 })
 export class NgxMintModule { }
